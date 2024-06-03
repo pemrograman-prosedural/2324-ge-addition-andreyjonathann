6ef164f92855728a2b98cc35b2ac26f733b97e27
andreyjonathann <143679440+andreyjonathann@users.noreply.github.com>
2024-06-03 17:42:24 +0700

    andrey

--- t02_01.c
+++ t02_01.c
@@ -1,12 +1,27 @@
-// NIM - Name
+// 12S23017 - Andrey jonathan nathaniel nainggolan
 // NIM - Name
 
 #include <stdio.h>
 
 int main(int _argv, char **_argc)
 {
-  //code
+int P,Q,M,N;
+ scanf("%d",&N);
+ 
+ for (int i =0; i<N; i++){
+  scanf("%d%d%d", P&Q&M);}
+ for (int j=0; j<M; j++);{
+  if (P>=Q){P=P/2;}
+  else {Q=Q/2;}
+ }
+ printf("%d%d",P,Q);
+ 
+
+  
+  
+
   
   return 0;
-
+  
 }
+
